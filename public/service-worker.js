32f8af4a8c7e58bd3bfdd58a6eadc6b1228f089f
Matt Hoover <matt@hoover.ml>
2019-09-10 05:26:20 -0400

    Handle failed network requests
    Add a `fetch` event handler to return `offline.html` for navigate
    requests

--- public/service-worker.js
+++ public/service-worker.js
@@ -58,5 +58,17 @@ evt.waitUntil(
 self.addEventListener('fetch', (evt) => {
   console.log('[ServiceWorker] Fetch', evt.request.url);
   // CODELAB: Add fetch event handler here.
-
+  if (evt.request.mode !== 'navigate') {
+    // Not a page navigation, bail.
+    return;
+  }
+  evt.respondWith(
+    fetch(evt.request)
+      .catch(() => {
+        return caches.open(CACHE_NAME)
+          .then((cache) => {
+            return cache.match('offline.html');
+          });
+      })
+  );
 });
