689b3d99e05b6030aef8574af6c63469760beee5
Matt Hoover <matt@hoover.ml>
2019-09-11 06:14:21 -0400

    Respond to User clicking the Install Button
    Invoke the `.prompt()` method of the saved `beforeinstallprompt` event,
    then hide the Install Button and log the User Response

--- public/scripts/install.js
+++ public/scripts/install.js
@@ -45,9 +45,20 @@ function saveBeforeInstallPromptEvent(evt) {
  */
 function installPWA(evt) {
   // CODELAB: Add code show install prompt & hide the install button.
+  deferredInstallPrompt.prompt();
+  // Hide the install button, it can't be called twice.
+  evt.srcElement.setAttribute('hidden', true);
 
   // CODELAB: Log user response to prompt.
-
+  deferredInstallPrompt.userChoice
+      .then((choice) => {
+        if (choice.outcome === 'accepted') {
+          console.log('User accepted the A2HS prompt', choice);
+        } else {
+          console.log('User dismissed the A2HS prompt', choice);
+        }
+        deferredInstallPrompt = null;
+      });
 }
 
 // CODELAB: Add event listener for appinstalled event
